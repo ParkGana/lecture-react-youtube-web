63f1a390b46724d4a84025ceb47e0c087a4f3cb8
DESKTOP-6P4PDN2\GANA <gnchoco97@gmail.com>
2022-03-16 04:15:46 +0900

    Video 모델 생성

---
+++ server/models/Video.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const videoSchema = mongoose.Schema({
+    uploader: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 100
+    },
+    description: {
+        type: String
+    },
+    privacy: {
+        type: Number,  // 0: private, 1 : public
+        default: 0
+    },
+    category: {
+        type: Number,  // 0: Film & Animation, 1: Autos & Vehicles, 2: Music, 3: Pets & Animals, 4: Sports
+        default: 0
+    },
+    duration: {
+        type: String
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    videoPath: {
+        type: String,
+        default: ''
+    },
+    thumbnailPath: {
+        type: String,
+        default: ''
+    }
+}, { timestamps: true });
+
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = { Video };
