2831f1776e2131c57cc1210c03a6aeb03d8dff21
DESKTOP-6P4PDN2\GANA <gnchoco97@gmail.com>
2022-03-22 11:55:19 +0900

    로그인 하지 않았을 경우 Subscribe, Upload 버튼 숨기기

--- client/src/components/views/Navbar/Sections/LeftMenu.js
+++ client/src/components/views/Navbar/Sections/LeftMenu.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Menu } from 'antd';
 
 
 function LeftMenu(props) {
-  return (
-    <Menu mode={props.mode}>
-      <Menu.Item key="subscribe">
-        <a href="/subscribe">Subscribe</a>
-      </Menu.Item>
-      <Menu.Item key="upload">
-        <a href="/video/upload">Upload</a>
-      </Menu.Item>
-    </Menu>
-  )
+  const user = useSelector(state => state.user);
+
+  if(user.userData) {
+    if(user.userData.isAuth) {
+      return (
+        <Menu mode={props.mode}>
+          <Menu.Item key="subscribe">
+            <a href="/subscribe">Subscribe</a>
+          </Menu.Item>
+          <Menu.Item key="upload">
+            <a href="/video/upload">Upload</a>
+          </Menu.Item>
+        </Menu>
+      )
+    }
+    else {
+      return (
+        <Menu mode={props.mode}>
+
+        </Menu>
+      )
+    }
+  }
+  else {
+    return (
+      <Menu mode={props.mode}>
+
+      </Menu>
+    )
+  }
 }
 
 export default LeftMenu;
